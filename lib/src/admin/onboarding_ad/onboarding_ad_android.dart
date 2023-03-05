01af2e905c21e4148229a522ba1b1e1d8f805ff1
viniciusddrft <viniciusddrft5@gmail.com>
2023-03-05 17:06:33 -0300

    pegando arquivos

--- lib/src/admin/onboarding_ad/onboarding_ad_android.dart
+++ lib/src/admin/onboarding_ad/onboarding_ad_android.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
 
 class OnboardingAdAndroid extends StatefulWidget {
   const OnboardingAdAndroid({super.key});
@@ -9,6 +10,8 @@ class OnboardingAdAndroid extends StatefulWidget {
 
 class _OnboardingAdAndroidState extends State<OnboardingAdAndroid> {
   final PageController _pageController = PageController();
+
+  late final FilePickerResult? files;
 
   @override
   Widget build(BuildContext context) {
@@ -29,56 +32,42 @@ class _OnboardingAdAndroidState extends State<OnboardingAdAndroid> {
                 Padding(
                   padding: const EdgeInsets.only(top: 50),
                   child: ElevatedButton(
-                      onPressed: () => _pageController.nextPage(
-                          duration: const Duration(milliseconds: 500),
-                          curve: Curves.elasticOut),
+                      onPressed: () async {
+                        files = await FilePicker.platform.pickFiles(
+                          type: FileType.image,
+                          allowMultiple: false,
+                        );
+                        _pageController.nextPage(
+                            duration: const Duration(milliseconds: 500),
+                            curve: Curves.elasticOut);
+                      },
                       child: const Text('Imagem')),
                 ),
                 Padding(
                   padding: const EdgeInsets.symmetric(vertical: 20),
                   child: ElevatedButton(
-                      onPressed: () => _pageController.nextPage(
-                          duration: const Duration(milliseconds: 500),
-                          curve: Curves.elasticOut),
+                      onPressed: () async {
+                        await FilePicker.platform.pickFiles(
+                          type: FileType.image,
+                          allowMultiple: true,
+                        );
+                        _pageController.nextPage(
+                            duration: const Duration(milliseconds: 500),
+                            curve: Curves.elasticOut);
+                      },
                       child: const Text('Duas imagens')),
                 ),
                 ElevatedButton(
-                    onPressed: () => _pageController.nextPage(
-                        duration: const Duration(milliseconds: 500),
-                        curve: Curves.elasticOut),
-                    child: const Text('Video')),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20, top: 70),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () => _pageController.previousPage(
+                    onPressed: () async {
+                      files = await FilePicker.platform.pickFiles(
+                        type: FileType.video,
+                        allowMultiple: false,
+                      );
+                      _pageController.nextPage(
                           duration: const Duration(milliseconds: 500),
-                          curve: Curves.elasticOut),
-                      icon: const Icon(Icons.arrow_back),
-                    ),
-                    const Text(
-                      'Selecione o arquivo',
-                      style: TextStyle(fontSize: 30),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 30),
-                  child: ElevatedButton.icon(
-                    label: const Text('pegar da galeria'),
-                    onPressed: () => _pageController.nextPage(
-                        duration: const Duration(milliseconds: 500),
-                        curve: Curves.elasticOut),
-                    icon: const Icon(Icons.file_upload_outlined),
-                  ),
-                ),
+                          curve: Curves.elasticOut);
+                    },
+                    child: const Text('Video')),
               ],
             ),
           ),

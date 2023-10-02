f923a0a6353deb941caf8fc164d2a771ee96bee5
viniciusddrft <viniciusddrft5@gmail.com>
2023-10-01 23:27:12 -0300

    :)

--- lib/src/modules/dashboard/ui/onboarding/onboarding_add_ad_web.dart
+++ lib/src/modules/dashboard/ui/onboarding/onboarding_add_ad_web.dart
@@ -1,10 +1,291 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class OnboardingAddAdWeb extends StatelessWidget {
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import '../../interector/dashboard_interector.dart';
+import '../../interector/dashboard_state.dart';
+
+class OnboardingAddAdWeb extends StatefulWidget {
   const OnboardingAddAdWeb({super.key});
 
   @override
+  State<OnboardingAddAdWeb> createState() => _OnboardingAddAdWebState();
+}
+
+class _OnboardingAddAdWebState extends State<OnboardingAddAdWeb> {
+  final image = ValueNotifier<File?>(null);
+  final image2 = ValueNotifier<File?>(null);
+  final hasTwoImages = ValueNotifier<bool>(false);
+  final dashboardInterector = Modular.get<DashboardInterector>();
+  final secondsController = TextEditingController();
+  final secondsFocus = FocusNode();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    dashboardInterector.addListener(() {
+      if (dashboardInterector.value is DashboardAddNewAd) {
+        showModalBottomSheet<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return const SizedBox(
+                height: 200,
+                child: Center(
+                  child: Text('Anúncio adicionado com successo!'),
+                ),
+              );
+            },
+            backgroundColor: Colors.grey);
+
+        Future.delayed(const Duration(seconds: 2),
+            () => Modular.to.navigate('/dashboard/dashboard'));
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    hasTwoImages.dispose();
+    image.dispose();
+    image2.dispose();
+    dashboardInterector.dispose();
+    secondsController.dispose();
+    secondsFocus.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Adicionar Anúncio'),
+      ),
+      body: AnimatedBuilder(
+        animation: Listenable.merge(
+            [hasTwoImages, image, image2, dashboardInterector]),
+        builder: (context, child) {
+          if (dashboardInterector.value is DashboardLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (dashboardInterector.value is DashboardFailed) {
+            return Center(
+              child:
+                  Text((dashboardInterector.value as DashboardFailed).message),
+            );
+          } else {
+            return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        height: 50.0,
+                        width: 150.0,
+                        child: RadioMenuButton<bool>(
+                          value: false,
+                          groupValue: hasTwoImages.value,
+                          onChanged: (value) => hasTwoImages.value = false,
+                          child: const Text('1 Imagem'),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50.0,
+                        width: 150.0,
+                        child: RadioMenuButton<bool>(
+                          value: true,
+                          groupValue: hasTwoImages.value,
+                          onChanged: (value) => hasTwoImages.value = true,
+                          child: const Text('2 Imagens'),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 25),
+                  child: GestureDetector(
+                    onTap: () async {
+                      final FilePickerResult? result =
+                          await FilePicker.platform.pickFiles(
+                        type: FileType.image,
+                      );
+
+                      final platformFile = result!.files.first;
+
+                      image.value = File(platformFile.path!);
+                    },
+                    onLongPress: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text('Deseja remover a imagem?'),
+                          actions: [
+                            TextButton(
+                              onPressed: () => Modular.to.pop(),
+                              child: const Text(
+                                'Cancelar',
+                                style: TextStyle(color: Colors.red),
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                image.value = null;
+                                Modular.to.pop();
+                              },
+                              child: const Text('Remover Imagem'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    child: SizedBox(
+                      width: 250,
+                      height: 150,
+                      child: Card(
+                        color: image.value == null
+                            ? Colors.white70
+                            : Colors.greenAccent,
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: image.value == null
+                                ? [
+                                    const Icon(Icons.upload_rounded),
+                                    const Text('Adiciona uma imagem.')
+                                  ]
+                                : [
+                                    const Icon(Icons.done),
+                                    const Text('Imagem Adicionado!')
+                                  ]),
+                      ),
+                    ),
+                  ),
+                ),
+                AnimatedSwitcher(
+                  switchInCurve: Curves.decelerate,
+                  switchOutCurve: Curves.decelerate,
+                  duration: const Duration(milliseconds: 700),
+                  child: hasTwoImages.value
+                      ? Padding(
+                          padding: const EdgeInsets.only(top: 25),
+                          child: GestureDetector(
+                            onTap: () async {
+                              final FilePickerResult? result =
+                                  await FilePicker.platform.pickFiles(
+                                type: FileType.image,
+                              );
+
+                              final platformFile = result!.files.first;
+
+                              image2.value = File(platformFile.path!);
+                            },
+                            onLongPress: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) => AlertDialog(
+                                  title: const Text('Deseja remover a imagem?'),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () => Modular.to.pop(),
+                                      child: const Text(
+                                        'Cancelar',
+                                        style: TextStyle(color: Colors.red),
+                                      ),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        image2.value = null;
+                                        Modular.to.pop();
+                                      },
+                                      child: const Text('Remover Imagem'),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                            child: SizedBox(
+                              width: 250,
+                              height: 150,
+                              child: Card(
+                                color: image2.value == null
+                                    ? Colors.white70
+                                    : Colors.greenAccent,
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: image2.value == null
+                                        ? [
+                                            const Icon(Icons.upload_rounded),
+                                            const Text('Adiciona uma imagem.')
+                                          ]
+                                        : [
+                                            const Icon(Icons.done),
+                                            const Text('Imagem Adicionado!')
+                                          ]),
+                              ),
+                            ),
+                          ),
+                        )
+                      : const SizedBox.shrink(),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 50),
+                  child: Text('Coloque o tempo de duração em segundos'),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  child: Form(
+                    key: _formKey,
+                    child: TextFormField(
+                      controller: secondsController,
+                      focusNode: secondsFocus,
+                      decoration: const InputDecoration(
+                        labelText: 'Duração ...',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(30),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 50),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      secondsFocus.unfocus();
+
+                      if (_formKey.currentState!.validate()) {
+                        if (hasTwoImages.value) {
+                          dashboardInterector.addAD(
+                              image: image.value!,
+                              image2: image2.value!,
+                              seconds: int.parse(secondsController.text));
+                        } else {
+                          dashboardInterector.addAD(
+                              image: image.value!,
+                              seconds: int.parse(secondsController.text));
+                        }
+                      }
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text('Adiconar anuncio'),
+                    ),
+                  ),
+                )
+              ],
+            );
+          }
+        },
+      ),
+    );
   }
 }
--- lib/src/modules/login/ui/register/register_web.dart
+++ lib/src/modules/login/ui/register/register_web.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import '../../interector/login_interector.dart';
+import 'package:local_ad_view/src/modules/login/interector/login_interector.dart';
 import '../../interector/login_state.dart';
 
 class RegisterWeb extends StatefulWidget {
@@ -13,7 +13,11 @@ class RegisterWeb extends StatefulWidget {
 class _RegisterWebState extends State<RegisterWeb> {
   final loginInteretor = Modular.get<LoginInteretor>();
   final emailController = TextEditingController();
+  final tokenController = TextEditingController();
   final passwordController = TextEditingController();
+  final _focusNodeEmail = FocusNode();
+  final _focusNodePassword = FocusNode();
+  final _focusNodeToken = FocusNode();
 
   @override
   void initState() {
@@ -33,6 +37,18 @@ class _RegisterWebState extends State<RegisterWeb> {
 
         Future.delayed(const Duration(seconds: 2),
             () => Modular.to.navigate('/login/login'));
+      } else if (loginInteretor.value is LoginFailed) {
+        showModalBottomSheet<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return const SizedBox(
+                height: 200,
+                child: Center(
+                  child: Text('Você não tem permissão para criar usuario!'),
+                ),
+              );
+            },
+            backgroundColor: Colors.grey);
       }
     });
     super.initState();
@@ -43,48 +59,34 @@ class _RegisterWebState extends State<RegisterWeb> {
     passwordController.dispose();
     emailController.dispose();
     loginInteretor.dispose();
+    tokenController.dispose();
+    _focusNodeEmail.dispose();
+    _focusNodePassword.dispose();
+    _focusNodeToken.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cadastro'),
         centerTitle: true,
-        automaticallyImplyLeading: false,
       ),
-      body: SizedBox(
-        height: size.height,
-        width: size.width,
-        child: Form(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: size.width * 0.3,
-                child: TextFormField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                    labelText: 'E-mail',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(30),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: size.height * 0.05),
-                child: SizedBox(
-                  width: size.width * 0.3,
-                  child: TextFormField(
-                    controller: passwordController,
+      body: ValueListenableBuilder(
+        valueListenable: loginInteretor,
+        builder: (context, value, child) => Center(
+          child: Form(
+            child: FractionallySizedBox(
+              widthFactor: 0.6,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    controller: emailController,
+                    focusNode: _focusNodeEmail,
                     decoration: const InputDecoration(
-                      labelText: 'Senha',
+                      labelText: 'E-mail',
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.all(
                           Radius.circular(30),
@@ -92,27 +94,56 @@ class _RegisterWebState extends State<RegisterWeb> {
                       ),
                     ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: size.height * 0.05),
-                child: SizedBox(
-                  width: size.width * 0.3,
-                  height: size.height * 0.06,
-                  child: ElevatedButton(
-                    onPressed: () => loginInteretor.register(
-                        email: emailController.text,
-                        password: passwordController.text,
-                        token: ''),
-                    child: const Center(
-                      child: Text(
-                        'Criar usuario',
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30, bottom: 60),
+                    child: TextFormField(
+                      controller: passwordController,
+                      focusNode: _focusNodePassword,
+                      decoration: const InputDecoration(
+                        labelText: 'Senha',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(30),
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              )
-            ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30, bottom: 60),
+                    child: TextFormField(
+                      controller: tokenController,
+                      focusNode: _focusNodeToken,
+                      decoration: const InputDecoration(
+                        labelText: 'Token',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(30),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 250,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        _focusNodeEmail.unfocus();
+                        _focusNodeEmail.unfocus();
+                        loginInteretor.register(
+                            email: emailController.text,
+                            password: passwordController.text,
+                            token: tokenController.text);
+                      },
+                      child: const Center(
+                        child: Text('Entrar'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
